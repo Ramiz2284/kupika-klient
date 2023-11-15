0a1dbb9bd26125abfd00eec648e2c06a96d2c500
Ramiz2284 <sultan22.84@mail.ru>
2023-11-15 23:39:04 +0300

    catBot

--- src/Componets/Pages/Home/Home.jsx
+++ src/Componets/Pages/Home/Home.jsx
@@ -1,9 +1,15 @@
 // HomePage.js
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import ChatbotPopup from '../Popup/ChatBotPopup'
 import styles from './Home.module.sass'
 
 const Home = () => {
+	const [isChatbotOpen, setIsChatbotOpen] = useState(false)
+
+	const openChatbot = () => setIsChatbotOpen(true)
+	const closeChatbot = () => setIsChatbotOpen(false)
+
 	return (
 		<div className={styles.container}>
 			<Link className={styles.Linkbutton} to='/create-list'>
@@ -13,7 +19,10 @@ const Home = () => {
 				<button className={styles.button}>Ваши списки</button>
 			</Link>
 
-			<button className={styles.button}>Спросить у бота</button>
+			<button className={styles.button} onClick={openChatbot}>
+				Спросить у бота
+			</button>
+			<ChatbotPopup isOpen={isChatbotOpen} onClose={closeChatbot} />
 		</div>
 	)
 }
