72238c5ff1583c401af718cb76fffcad79b1fccb
Ramiz2284 <sultan22.84@mail.ru>
2024-05-07 14:48:14 +0300

    add whatsapo btn

--- src/Componets/Pages/Home/Home.jsx
+++ src/Componets/Pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 // HomePage.js
 import React, { useState } from 'react'
+import { FaWhatsapp } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 import ChatbotPopup from '../Popup/ChatBotPopup'
 import styles from './Home.module.sass'
@@ -23,6 +24,20 @@ const Home = () => {
 				Спросить у бота
 			</button>
 			<ChatbotPopup isOpen={isChatbotOpen} onClose={closeChatbot} />
+			<a
+				className={styles.Linkbutton}
+				href='https://wa.me/+905444558407'
+				target='_blank'
+				rel='noopener noreferrer'
+			>
+				<button className={styles.button}>
+					<FaWhatsapp /> Копченная рыба в Анталии
+				</button>
+			</a>
+			<p className={styles.explanation}>
+				Нажмите на эту кнопку, чтобы перейти в WhatsApp и просмотреть наш
+				ассортимент, узнать цены и сделать заказ.
+			</p>
 		</div>
 	)
 }
