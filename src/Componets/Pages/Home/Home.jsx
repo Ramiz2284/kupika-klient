74bbb30c2fc708f9ea1434ca9dd1eaa7fa9bccea
Ramiz2284 <sultan22.84@mail.ru>
2024-06-03 16:02:13 +0300

    add H1 styles

--- src/Componets/Pages/Home/Home.jsx
+++ src/Componets/Pages/Home/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
 
 	return (
 		<div className={styles.container}>
-			<h1>Список покупок для магазина</h1>
+			<h1 className={styles.homeH1}>Список покупок для магазина</h1>
 			<Link className={styles.Linkbutton} to='/create-list'>
 				<button className={styles.button}>Создать список</button>
 			</Link>
